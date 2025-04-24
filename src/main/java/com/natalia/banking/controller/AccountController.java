9c76e85132454d2e3faaa2be398184c4ba6c5138
Natalia Salvino <nataliasalvino86@gmail.com>
2025-04-24 19:51:08 -0300

    adding basic project structure

---
+++ src/main/java/com/natalia/banking/controller/AccountController.java
@@ -0,0 +1,9 @@
+package com.natalia.banking.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/conta")
+public class AccountController {
+}
