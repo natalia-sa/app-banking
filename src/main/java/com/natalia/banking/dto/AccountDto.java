7f925c26c0e5fc4d3ff2b67a6a72f8f0af1cd2aa
Natalia Salvino <nataliasalvino86@gmail.com>
2025-04-24 21:15:34 -0300

    add schema examples

--- src/main/java/com/natalia/banking/dto/AccountDto.java
+++ src/main/java/com/natalia/banking/dto/AccountDto.java
@@ -2,12 +2,15 @@ package com.natalia.banking.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.natalia.banking.model.Account;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record AccountDto(
         @JsonProperty("numero_conta")
+        @Schema(description = "Número da conta", example = "1234")
         Integer number,
 
         @JsonProperty("saldo")
+        @Schema(description = "Saldo da conta", example = "100.50")
         Double balance
 ) {
         public AccountDto(Account savedAccount) {
--- src/main/java/com/natalia/banking/dto/TransactionRequestDto.java
+++ src/main/java/com/natalia/banking/dto/TransactionRequestDto.java
@@ -2,18 +2,20 @@ package com.natalia.banking.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.natalia.banking.enums.TransactionType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record TransactionRequestDto(
 
         @JsonProperty("forma_pagamento")
+        @Schema(description = "Forma de pagamento da transação", example = "P")
         TransactionType transactionType,
 
-
         @JsonProperty("numero_conta")
+        @Schema(description = "Número da conta para a transação", example = "1234")
         Integer accountNumber,
 
-
         @JsonProperty("valor")
+        @Schema(description = "Valor da transação", example = "150.00")
         Double value
 ) {
 }
